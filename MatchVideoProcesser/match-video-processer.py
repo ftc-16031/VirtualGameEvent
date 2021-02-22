41ce5d40f7f9228a0b027c2b864174885291f498
Kevin Zhou <kevin.zhou@gmi-mr.com>
2021-02-21 23:30:50 -0800

    add event feature and automatically switch tabs

--- MatchVideoProcesser/match-video-processer.py
+++ MatchVideoProcesser/match-video-processer.py
@@ -38,8 +38,14 @@ def mmss_to_seconds(mmss):
         return mmss
 
 
+class InvalidEventException(Exception):
+    def __init__(self, message):
+        self.message = message
+
 
 class MatchVideoProcessor(QtWidgets.QMainWindow):
+
+    game_start_offset = None
 
     def __init__(self, media_file=None, master=None):
         QtWidgets.QMainWindow.__init__(self, master)
@@ -56,10 +62,35 @@ class MatchVideoProcessor(QtWidgets.QMainWindow):
 
         self.create_ui()
 
+        self.is_paused = False
+
         if media_file is not None:
             self.open_media_file(media_file)
 
-        self.is_paused = False
+    def ring_goal_event_widgets(self):
+        radiobutton = QtWidgets.QRadioButton("Launched Rings into Goals")
+        ring_goals = QtWidgets.QHBoxLayout()
+        high_goal = QtWidgets.QSpinBox()
+        high_goal.setRange(0, 3)
+        high_goal.setValue(0)
+        ring_goals.addStretch(1)
+        ring_goals.addWidget(QtWidgets.QLabel('High: '))
+        ring_goals.addWidget(high_goal)
+        ring_goals.addStretch(1)
+        medium_goal = QtWidgets.QSpinBox()
+        medium_goal.setRange(0, 3)
+        medium_goal.setValue(0)
+        ring_goals.addWidget(QtWidgets.QLabel('Medium: '))
+        ring_goals.addWidget(medium_goal)
+        ring_goals.addStretch(1)
+        low_goal = QtWidgets.QSpinBox()
+        low_goal.setRange(0, 3)
+        low_goal.setValue(0)
+        ring_goals.addWidget(QtWidgets.QLabel('Low: '))
+        ring_goals.addWidget(low_goal)
+        ring_goals.addStretch(1)
+        associated_widgets = {'high': high_goal, 'medium': medium_goal, 'low': low_goal}
+        return ring_goals, radiobutton, associated_widgets
 
     def create_ui(self):
         """Set up the user interface, signals & slots
@@ -103,56 +134,103 @@ class MatchVideoProcessor(QtWidgets.QMainWindow):
 
         self.eventstabs = QtWidgets.QTabWidget()
 
-        self.gamestart_tab = QtWidgets.QVBoxLayout()
-        self.gamestart_rb = QtWidgets.QRadioButton("Game Start")
-        self.gamestart_tab.addWidget(self.gamestart_rb)
+        self.events=[]
+        self.events.append([])
+        tab = QtWidgets.QVBoxLayout()
+        radiobutton = QtWidgets.QRadioButton("Game Start")
+        tab.addWidget(radiobutton)
+        # automatically select the game start button
+        radiobutton.setChecked(True)
+        self.events[0].append({'radio_button': radiobutton, 'handler': self.game_start_event, 'associated_widgets': {}})
         tab_widget = QtWidgets.QWidget()
-        tab_widget.setLayout(self.gamestart_tab)
+        tab_widget.setLayout(tab)
         self.eventstabs.addTab(tab_widget, 'Game Start')
 
-        self.autonomous_tab = QtWidgets.QVBoxLayout()
-        self.autonomous_tab.addWidget(QtWidgets.QRadioButton("Power Shot Target Knocked"))
-        self.autonomous_tab.addWidget(QtWidgets.QRadioButton("Wobble Goal Delivered to Target Zone"))
-        self.autonomous_tab.addWidget(QtWidgets.QRadioButton("Launched Rings into Low Goal"))
-        self.autonomous_tab.addWidget(QtWidgets.QRadioButton("Launched Rings into Medium Goal"))
-        self.autonomous_tab.addWidget(QtWidgets.QRadioButton("Launched Rings into High Goal"))
-        self.autonomous_tab.addWidget(QtWidgets.QRadioButton("Robot Parked"))
+        self.events.append([])
+        tab = QtWidgets.QVBoxLayout()
+        # wobble goal
+        radiobutton = QtWidgets.QRadioButton("Wobble Goal Delivered to Target Zone")
+        tab.addWidget(radiobutton)
+        self.events[1].append({'radio_button': radiobutton, 'handler': self.wobblegoal_target_event, 'associated_widgets': {}})
+        # parking
+        radiobutton = QtWidgets.QRadioButton("Robot Parked")
+        tab.addWidget(radiobutton)
+        self.events[1].append({'radio_button': radiobutton, 'handler': self.robot_park_event, 'associated_widgets': {}})
+        # ring goal
+        ring_goals, radiobutton, associated_widgets = self.ring_goal_event_widgets()
+        tab.addWidget(radiobutton)
+        tab.addLayout(ring_goals)
+        self.events[1].append({'radio_button': radiobutton, 'handler': self.ring_goal_auto_event, 'associated_widgets': associated_widgets})
+        # power shot
+        radiobutton = QtWidgets.QRadioButton("Power Shot Target Knocked")
+        tab.addWidget(radiobutton)
+        self.events[1].append({'radio_button': radiobutton, 'handler': self.powershot_event, 'associated_widgets': {}})
+        # tab
         tab_widget = QtWidgets.QWidget()
-        tab_widget.setLayout(self.autonomous_tab)
+        tab_widget.setLayout(tab)
         self.eventstabs.addTab(tab_widget, 'Autonomous')
 
-        self.teleop_tab = QtWidgets.QVBoxLayout()
-        self.teleop_tab.addWidget(QtWidgets.QRadioButton("Launched Rings into Low Goal"))
-        self.teleop_tab.addWidget(QtWidgets.QRadioButton("Launched Rings into Medium Goal"))
-        self.teleop_tab.addWidget(QtWidgets.QRadioButton("Launched Rings into High Goal"))
+        self.events.append([])
+        tab = QtWidgets.QVBoxLayout()
+        ring_goals, radiobutton, associated_widgets = self.ring_goal_event_widgets()
+        tab.addWidget(radiobutton)
+        tab.addLayout(ring_goals)
+        self.events[2].append({'radio_button': radiobutton, 'handler': self.ring_goal_teleop_event, 'associated_widgets': associated_widgets})
         tab_widget = QtWidgets.QWidget()
-        tab_widget.setLayout(self.teleop_tab)
+        tab_widget.setLayout(tab)
         self.eventstabs.addTab(tab_widget, 'Teleop')
 
-        self.endgame_tab = QtWidgets.QVBoxLayout()
-        self.endgame_tab.addWidget(QtWidgets.QRadioButton("Power Shot Target Knocked"))
-        self.endgame_tab.addWidget(QtWidgets.QRadioButton("Wobble Goal Delivered to Start Line"))
-        self.endgame_tab.addWidget(QtWidgets.QRadioButton("Wobble Goal Delivered to Drop Zone"))
-        self.endgame_tab.addWidget(QtWidgets.QRadioButton("Launched Rings into Low Goal"))
-        self.endgame_tab.addWidget(QtWidgets.QRadioButton("Launched Rings into Medium Goal"))
-        self.endgame_tab.addWidget(QtWidgets.QRadioButton("Launched Rings into High Goal"))
-        self.endgame_tab.addWidget(QtWidgets.QRadioButton("Rings Supported by Wobble Goal"))
+        self.events.append([])
+        tab = QtWidgets.QVBoxLayout()
+        # wobble goal to start line
+        radiobutton = QtWidgets.QRadioButton("Wobble Goal Delivered to Start Line")
+        tab.addWidget(radiobutton)
+        self.events[3].append({'radio_button': radiobutton, 'handler': self.wobblegoal_startline_event, 'associated_widgets': {}})
+        # wobble goal to drop zone
+        radiobutton = QtWidgets.QRadioButton("Wobble Goal Delivered to Drop Zone")
+        tab.addWidget(radiobutton)
+        self.events[3].append({'radio_button': radiobutton, 'handler': self.wobblegoal_dropzone_event, 'associated_widgets': {}})
+        # ring goal
+        ring_goals, radiobutton, associated_widgets = self.ring_goal_event_widgets()
+        tab.addWidget(radiobutton)
+        tab.addLayout(ring_goals)
+        self.events[3].append({'radio_button': radiobutton, 'handler': self.ring_goal_teleop_event, 'associated_widgets': associated_widgets})
+        # power shot
+        radiobutton = QtWidgets.QRadioButton("Power Shot Target Knocked")
+        tab.addWidget(radiobutton)
+        self.events[3].append({'radio_button': radiobutton, 'handler': self.powershot_event, 'associated_widgets': {}})
+        # tab
         tab_widget = QtWidgets.QWidget()
-        tab_widget.setLayout(self.endgame_tab)
+        tab_widget.setLayout(tab)
         self.eventstabs.addTab(tab_widget, 'End Game')
 
-        self.penalty_tab = QtWidgets.QVBoxLayout()
-        self.penalty_tab.addWidget(QtWidgets.QRadioButton("Minor Penalty"))
-        self.penalty_tab.addWidget(QtWidgets.QRadioButton("Major Penalty"))
+        self.events.append([])
+        tab = QtWidgets.QVBoxLayout()
+        penalty_layout = QtWidgets.QHBoxLayout()
+        radiobutton = QtWidgets.QRadioButton("Minor Penalty")
+        penalty_layout.addWidget(radiobutton)
+        ring_goals.addStretch(1)
+        reason_edit = QtWidgets.QLineEdit()
+        penalty_layout.addWidget(reason_edit)
+        ring_goals.addStretch(1)
+        tab.addLayout(penalty_layout)
+        self.events[4].append({'radio_button': radiobutton, 'handler': self.minor_penalty_event, 'associated_widgets': {'reason': reason_edit}})
+        penalty_layout = QtWidgets.QHBoxLayout()
+        radiobutton = QtWidgets.QRadioButton("Major Penalty")
+        penalty_layout.addWidget(radiobutton)
+        ring_goals.addStretch(1)
+        reason_edit = QtWidgets.QLineEdit()
+        penalty_layout.addWidget(reason_edit)
+        ring_goals.addStretch(1)
+        tab.addLayout(penalty_layout)
+        self.events[4].append({'radio_button': radiobutton, 'handler': self.major_penalty_event, 'associated_widgets': {'reason': reason_edit}})
         tab_widget = QtWidgets.QWidget()
-        tab_widget.setLayout(self.penalty_tab)
+        tab_widget.setLayout(tab)
         self.eventstabs.addTab(tab_widget, 'Penalty')
 
         self.htablebox.addWidget(self.eventstabs, stretch=6)
         self.eventstable = QtWidgets.QTableWidget(2, 2)
         self.eventstable.setHorizontalHeaderLabels(['Event', 'Points'])
-        self.eventstable.setVerticalHeaderLabels(['--:--', 'Total'])
-        self.eventstable.setItem(1, 1, QtWidgets.QTableWidgetItem('0'))
         header = self.eventstable.horizontalHeader()
         header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
         header.setSectionResizeMode(1, QtWidgets.QHeaderView.ResizeToContents)
@@ -184,6 +262,8 @@ class MatchVideoProcessor(QtWidgets.QMainWindow):
         self.timer.setInterval(100)
         self.timer.timeout.connect(self.update_ui)
 
+        self.reset()
+
     def play_pause(self):
         """Toggle play/pause status
         """
@@ -214,13 +294,101 @@ class MatchVideoProcessor(QtWidgets.QMainWindow):
         self.eventstable.insertRow(0)
         self.eventstable.setVerticalHeaderLabels(['--:--', 'Total'])
         self.eventstable.setItem(1, 1, QtWidgets.QTableWidgetItem('0'))
+        self.eventstabs.setTabEnabled(0, True)
+        self.eventstabs.setTabEnabled(1, False)
+        self.eventstabs.setTabEnabled(2, False)
+        self.eventstabs.setTabEnabled(3, False)
+        self.eventstabs.setTabEnabled(4, True)
+        self.eventstabs.setCurrentIndex(0)
+        self.game_start_offset = None
+
+    def game_start_event(self, radiobutton, timestamp, associated_widgets):
+        self.game_start_offset = timestamp
+        self.eventstabs.setTabEnabled(0, False)
+        self.eventstabs.setTabEnabled(1, True)
+        self.eventstabs.setTabEnabled(2, False)
+        self.eventstabs.setTabEnabled(3, False)
+        self.eventstabs.setCurrentIndex(1)
+        return radiobutton.text(), 0, timestamp
+
+    def powershot_event(self, radiobutton, timestamp, associated_widgets):
+        return radiobutton.text(), 15, timestamp
+
+    def wobblegoal_target_event(self, radiobutton, timestamp, associated_widgets):
+        return radiobutton.text(), 15, timestamp
+
+    def wobblegoal_startline_event(self, radiobutton, timestamp, associated_widgets):
+        return radiobutton.text(), 5, timestamp
+
+    def wobblegoal_dropzone_event(self, radiobutton, timestamp, associated_widgets):
+        return radiobutton.text(), 20, timestamp
+
+    def ring_goal_event(self, radiobutton, timestamp, associated_widgets, points_schema):
+        low_goal_point, medium_goal_point, high_goal_point = points_schema
+        text = radiobutton.text() + ','
+        total_points = 0
+        if associated_widgets['high'].value() > 0:
+            total_points += high_goal_point * associated_widgets['high'].value()
+            text += f" high ({associated_widgets['high'].value()})"
+        if associated_widgets['medium'].value() > 0:
+            total_points += medium_goal_point * associated_widgets['medium'].value()
+            text += f" medium ({associated_widgets['medium'].value()})"
+        if associated_widgets['low'].value() > 0:
+            total_points += low_goal_point * associated_widgets['low'].value()
+            text += f" low ({associated_widgets['low'].value()})"
+        if total_points == 0:
+            raise InvalidEventException('Please specify number of rings launched into goals!')
+        return text, total_points, timestamp
+
+    def ring_goal_auto_event(self, radiobutton, timestamp, associated_widgets):
+        points_schema = (3, 6, 12)
+        return self.ring_goal_event(radiobutton, timestamp, associated_widgets, points_schema)
+
+    def ring_goal_teleop_event(self, radiobutton, timestamp, associated_widgets):
+        points_schema = (2, 4, 6)
+        return self.ring_goal_event(radiobutton, timestamp, associated_widgets, points_schema)
+
+    def robot_park_event(self, radiobutton, timestamp, associated_widgets):
+        return radiobutton.text(), 5, timestamp
+
+    def major_penalty_event(self, radiobutton, timestamp, associated_widgets):
+        text = radiobutton.text()
+        if len(associated_widgets['reason'].text()) > 0:
+            text += f", {associated_widgets['reason'].text()}"
+        else:
+            raise InvalidEventException('Please specify a reason for the penalty!')
+        return text, -30, timestamp
+
+    def minor_penalty_event(self, radiobutton, timestamp, associated_widgets):
+        text = radiobutton.text()
+        if len(associated_widgets['reason'].text()) > 0:
+            text += f", {associated_widgets['reason'].text()}"
+        else:
+            raise InvalidEventException('Please specify a reason for the penalty!')
+        return text, -10, timestamp
 
     def add_event(self):
         """ Add the event
         """
-        seconds = 12
-        event = 'Game Start'
-        point = 10
+        # check if there is a event selected
+        current_tab = self.eventstabs.currentIndex()
+        for event in self.events[current_tab]:
+            if event['radio_button'].isChecked():
+                timestamp = int(self.mediaplayer.get_time() / 1000)
+                try:
+                    event_text, point, seconds = event['handler'](event['radio_button'], timestamp, event['associated_widgets'])
+                except InvalidEventException as ex:
+                    msgBox = QtWidgets.QMessageBox()
+                    msgBox.setText(ex.message)
+                    msgBox.exec_()
+                    return
+                self.update_events_table(seconds, event_text, point)
+                return
+        msgBox = QtWidgets.QMessageBox()
+        msgBox.setText("Please select an event to add !")
+        msgBox.exec_()
+
+    def update_events_table(self, seconds, event, point):
         target_row_no = None
         for row_no in range (self.eventstable.rowCount()):
             row_name = self.eventstable.verticalHeaderItem(row_no).text()
@@ -235,14 +403,14 @@ class MatchVideoProcessor(QtWidgets.QMainWindow):
                 # target row not found yet, compare the time and see if we need to insert
                 row_time = mmss_to_seconds(row_name)
                 if row_time > seconds:
-                    target_row_no = row_no
+                    target_row_no = row_no + 1
                     self.eventstable.insertRow(target_row_no)
             else:
                 # target row already found, pass
                 pass
         if target_row_no is None:
             # insert the row to the end
-            target_row_no = self.eventstable.rowCount() - 2
+            target_row_no = self.eventstable.rowCount() - 1
             self.eventstable.insertRow(target_row_no)
         # update target row
         self.eventstable.setVerticalHeaderItem(target_row_no, QtWidgets.QTableWidgetItem(seconds_to_mmss(seconds)))
@@ -316,15 +484,40 @@ class MatchVideoProcessor(QtWidgets.QMainWindow):
         self.positionslider.setValue(media_pos)
         self.progress.setText(f"{ms_to_mmss(self.mediaplayer.get_time())} / {ms_to_mmss(self.mediaplayer.get_length())}")
 
+        # automatically switch the events tab status based on game start offset
+        if self.game_start_offset is not None:
+            seconds_from_game_start = int(self.mediaplayer.get_time()/1000) - self.game_start_offset
+            if 0 <= seconds_from_game_start < 35:
+                # enable the 'autonomous' tab
+                self.eventstabs.setTabEnabled(1, True)
+                # self.eventstabs.setCurrentIndex(1)
+            if 0 <= seconds_from_game_start < 25:
+                # disable the 'teleop' and 'end game' tab
+                self.eventstabs.setTabEnabled(2, False)
+                self.eventstabs.setTabEnabled(3, False)
+            if 25 <= seconds_from_game_start < 125:
+                # enable the 'teleop' tab
+                self.eventstabs.setTabEnabled(2, True)
+                if seconds_from_game_start == 30:
+                    self.eventstabs.setCurrentIndex(2)
+            if 35 <= seconds_from_game_start < 115:
+                # disable the 'autonomous' and 'end game' tab
+                self.eventstabs.setTabEnabled(1, False)
+                self.eventstabs.setTabEnabled(3, False)
+            if 115 <= seconds_from_game_start < 155:
+                # enable the 'end game' tab
+                self.eventstabs.setTabEnabled(3, True)
+                if seconds_from_game_start == 120:
+                    self.eventstabs.setCurrentIndex(3)
+            if 125 <= seconds_from_game_start < 155:
+                # disable the 'autonomous' and 'teleop' tab
+                self.eventstabs.setTabEnabled(1, False)
+                self.eventstabs.setTabEnabled(2, False)
+
         # No need to call this function if nothing is played
         if not self.mediaplayer.is_playing():
             self.timer.stop()
 
-            # After the video finished, the play button stills shows "Pause",
-            # which is not the desired behavior of a media player.
-            # This fixes that "bug".
-            if not self.is_paused:
-                self.reset()
 
 def main():
     """Entry point for our processor
