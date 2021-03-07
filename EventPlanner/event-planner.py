82634c45dac2c224bd162cc7280a4b15ce27c7f3
Kevin Zhou <zhoukang@gmail.com>
2021-03-07 00:40:21 -0800

    Read from FTC db

--- EventPlanner/event-planner.py
+++ EventPlanner/event-planner.py
@@ -26,7 +26,7 @@ class EventPlanner(QtWidgets.QMainWindow):
         self.create_ui()
 
         if db_file is not None:
-            self.open_db(db_file)
+            self.read_from_db(db_file)
 
     def create_ui(self):
         """Set up the user interface, signals
@@ -34,9 +34,20 @@ class EventPlanner(QtWidgets.QMainWindow):
         self.widget = QtWidgets.QWidget(self)
         self.setCentralWidget(self.widget)
 
+        self.matchstable = QtWidgets.QTableWidget(0, 4)
+        self.matchstable.setHorizontalHeaderLabels(['Red', 'Red', 'Blue', 'Blue'])
+        header = self.matchstable.horizontalHeader()
+        header.setSectionResizeMode(0, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(2, QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(3, QtWidgets.QHeaderView.Stretch)
+
         self.vboxlayout = QtWidgets.QVBoxLayout()
+        self.vboxlayout.addWidget(self.matchstable, stretch=10)
 
         self.widget.setLayout(self.vboxlayout)
+
+
 
         menu_bar = self.menuBar()
 
@@ -56,11 +67,15 @@ class EventPlanner(QtWidgets.QMainWindow):
         self.timer.setInterval(5000)
         self.timer.timeout.connect(self.update_ui)
 
-        self.reset()
+        self.update_ui()
 
     def reset(self):
-        """ Reset UI
+        """ Reset
         """
+        self.quals = []
+        self.teams = []
+        for row_no in range(self.matchstable.rowCount()):
+            self.eventstable.removeRow(0)
         pass
 
     def open_file(self):
@@ -73,15 +88,50 @@ class EventPlanner(QtWidgets.QMainWindow):
             return
 
         # getOpenFileName returns a tuple, so use only the actual file name
-        self.open_db(filename[0])
+        self.read_from_db(filename[0])
 
-    def open_db(self, filename):
+    def get_team_info(self, team_number):
+        for row in self.teams:
+            if row['number'] == team_number:
+                return row
+        raise Exception(f'Team {team_number} not found')
+
+    def read_from_db(self, filename):
         self.db_file = filename
-        self.conn = sqlite3.connect(self.db_file)
-        cur = self.conn.cursor()
-        r = cur.execute("SELECT * FROM quals")
-        print(r.fetchone())
-        pass
+        conn = sqlite3.connect(self.db_file)
+        conn.row_factory = sqlite3.Row
+        self.reset()
+        cur = conn.cursor()
+        result = cur.execute("SELECT * FROM quals")
+        self.quals = result.fetchall()
+        result = cur.execute("SELECT * FROM teamInfo")
+        self.teams = result.fetchall()
+        conn.close()
+        row_no = 0
+        for match in self.quals:
+            self.matchstable.insertRow(row_no)
+            self.matchstable.setVerticalHeaderItem(row_no, QtWidgets.QTableWidgetItem(f'#{match["match"]}'))
+            team = self.get_team_info(match['red1'])
+            item = QtWidgets.QTableWidgetItem(f"{team['number']} : {team['name']}")
+            item.setFlags(item.flags() ^ QtCore.Qt.ItemIsEditable)
+            item.setForeground(QtGui.QBrush(QtGui.QColor(QtCore.Qt.red)))
+            self.matchstable.setItem(row_no, 0, item)
+            team = self.get_team_info(match['red2'])
+            item = QtWidgets.QTableWidgetItem(f"{team['number']} : {team['name']}")
+            item.setFlags(item.flags() ^ QtCore.Qt.ItemIsEditable)
+            item.setForeground(QtGui.QBrush(QtGui.QColor(QtCore.Qt.red)))
+            self.matchstable.setItem(row_no, 1, item)
+            team = self.get_team_info(match['blue1'])
+            item = QtWidgets.QTableWidgetItem(f"{team['number']} : {team['name']}")
+            item.setFlags(item.flags() ^ QtCore.Qt.ItemIsEditable)
+            item.setForeground(QtGui.QBrush(QtGui.QColor(QtCore.Qt.blue)))
+            self.matchstable.setItem(row_no, 2, item)
+            team = self.get_team_info(match['blue2'])
+            item = QtWidgets.QTableWidgetItem(f"{team['number']} : {team['name']}")
+            item.setFlags(item.flags() ^ QtCore.Qt.ItemIsEditable)
+            item.setForeground(QtGui.QBrush(QtGui.QColor(QtCore.Qt.blue)))
+            self.matchstable.setItem(row_no, 3, item)
+            row_no += 1
 
     def update_ui(self):
         """Updates the user interface"""
@@ -101,6 +151,7 @@ def main():
             print(f'ERROR : DB file passed in [{filename}] not exists')
     player = EventPlanner(db_file=db_file)
     player.show()
+    player.resize(640, 480)
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
